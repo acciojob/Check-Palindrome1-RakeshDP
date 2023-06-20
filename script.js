31e0cc6092cf9b01f5651058440f8fd6186f7ebc
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-20 14:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // complete the given function
 
 function palindrome(str){
-	let n=str.length;
-	let newStr= str.split("");
+	// Remove non-alphanumeric characters and convert to lowercase
+  const cleanedString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  let left = 0;
+  let right = cleanedString.length - 1;
+
+  while (left < right) {
+    if (cleanedString[left] !== cleanedString[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+	// let n=str.length;
+	// let newStr= str.split("");
 	
-	let start =0;
-	let end=n-1;
-	while(start > end){
-		if(start.charAt[start] === end.charAt[end]){
-			start++;
-			end--;
-		}
-		else {
-			return false;
-		}
-		return true;
-	}
+	// let start =0;
+	// let end=n-1;
+	// while(start > end){
+	// 	if(start.charAt[start] === end.charAt[end]){
+	// 		start++;
+	// 		end--;
+	// 	}
+	// 	else {
+	// 		return false;
+	// 	}
+	// 	return true;
+	// }
 	
 }
 module.exports = palindrome
