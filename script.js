651bd216bdb3b4fd8ef6e9624be74f0c11d20a81
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-20 14:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
-
+	let n=str.length;
+	let newStr= str.split("");
+	
+	let start =0;
+	let end=n-1;
+	while(start > end){
+		if(start.charAt[start] === end.charAt[end]){
+			start++;
+			end--;
+		}
+		else {
+			return false;
+		}
+		return true;
+	}
+	
 }
 module.exports = palindrome
